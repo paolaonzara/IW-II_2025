32393236db32e856ca1307217fe368d468ae1d66
Paola Onório Zaratini <159515333+paolaonzara@users.noreply.github.com>
2025-02-27 11:02:13 -0300

    Add files via upload

---
+++ aula03/processo01.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = $_GET['cxnome'];
+    $msg = $_GET['cxmsg'];
+
+    echo "Nome digitado: $nome <br>";
+    echo "Obrigado por digitar sua mensagem! sua mensagem é: $msg <br>";
+
+
+?>
